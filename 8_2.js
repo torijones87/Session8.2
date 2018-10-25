db8580eea1e41c4982d0fcd1f87b9819f0a339e3
Tori Jones <Tori@Trystian-Amedee-Joness-MacBook-Pro.local>
2018-10-25 18:49:07 -0500

    done

---
+++ 8_2.js
@@ -0,0 +1,9 @@
+function prepare(tablenumber, items){
+    this.tablenumber = tablenumber;
+    this.items = items;
+}
+
+var prepare1 = new prepareFood ("3", "[burger, pasta, coke]");
+Var prepare2 = new prepareFood("3", "[Burger, Pasta, Coke]");
+    console.log("Preparing Food for items" + prepareFood1.type);
+    console.log("Serving Food for items " + serveFood.type);
